8582bbd4fd1d3b1f254f594e90e24ab6684f69f8
Yang Kyung Hun <ygh1254@hanyang.ac.kr>
2023-10-13 21:18:49 +0900

    feat: POST test

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import requests
 import gradio as gr
 
+port = 0
+
 def input(departure, arrival, year, month, day):
     result = f"Departure is {departure} and arrival is {arrival}\nYour flight is {int(year)}-{int(month)}-{int(day)}"
+    res = requests.post(
+        f"http://127.0.0.1:{port}/upload/",
+        data=[departure, arrival, int(year), int(month), int(day)]
+    )
     return result
 
 demo = gr.Interface(
