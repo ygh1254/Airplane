a4779d56b94e03eadb5a4258e8ab2c987e3d084f
Yang Kyung Hun <ygh1254@hanyang.ac.kr>
2023-10-13 16:27:02 +0900

    feat: gradio demo

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-import gradio
+import gradio as gr
+
+def input(departure, arrival, year, month, day):
+    result = f"Departure is {departure} and arrival is {arrival}\nYour flight is {int(year)}-{int(month)}-{int(day)}"
+    return result
+
+demo = gr.Interface(
+    input,
+    [
+        gr.Dropdown(
+            ["ICN", "KIX", "CTS"], label="Departure"
+        ),
+        gr.Dropdown(
+            ["ICN", "KIX", "CTS"], label="Arrival"
+        ),
+        gr.Number(
+            label="year"
+        ),
+        gr.Number(
+            label="month"
+        ),
+        gr.Number(
+            label="day"
+        )
+    ],
+    'text'
+)
+
+demo.launch()
